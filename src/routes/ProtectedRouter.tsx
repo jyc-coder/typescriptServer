1c41ddf24897e941fae8689d4ee9cde341fd9b0c
jyc-coder <jyc4648@gmail.com>
2023-04-06 23:23:17 +0900

    🐎 feat : Add optional rendering in ProtectedRouter
    - Write conditional rendering code to hide the component if the token doesn't exist.

--- src/routes/ProtectedRouter.tsx
+++ src/routes/ProtectedRouter.tsx
@@ -4,8 +4,9 @@ import { getCookie } from '../utils/cookies'
 import { Outlet, useNavigate } from 'react-router-dom'
 
 export default function ProtectedRouter() {
-    const isAuthenticated = verifyToken()
     const navigate = useNavigate()
+    const token = getCookie('accessToken')
+    const isAuthenticated = verifyToken()
 
     useEffect(() => {
         if (isAuthenticated === 'FAILED') {
@@ -15,5 +16,9 @@ export default function ProtectedRouter() {
         }
     }, [isAuthenticated])
 
+    // 토큰이 없다면 verifyToken 이 작동할 때까지 잠시 컴포넌트를 숨김
+    // 토큰이 있으면 verifyToken 작동 전이라도 일단 컴포넌트를 보여주되
+    // 만약 그게 잘못된 녀석이라면, 백엔드에 요청을 못보내기에 컨텐츠를 볼수는 없다.
+    if (!token && isAuthenticated !== 'SUCCESS') return <>로딩 중</>
     return <Outlet />
 }
