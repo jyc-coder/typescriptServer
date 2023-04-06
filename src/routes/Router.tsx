cd407ba8bf79ed8ce980ba1d39de38678aa8354a
jyc-coder <jyc4648@gmail.com>
2023-04-06 23:16:05 +0900

    📝 feat : Move ProtectedRouter
    - change the boundary of ProtectedRouter

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,14 +3,19 @@ import LoginPage from '../pages/LoginPage'
 import PostCreatePage from '../pages/PostCreatePage'
 import PostsPage from '../pages/PostsPage'
 import ProtectedRouter from './ProtectedRouter'
+import { Link } from 'react-router-dom'
 
 function Router() {
     return (
         <BrowserRouter>
+            <nav>
+                <Link to="/posts">포스트</Link> | <Link to="/posts/new">새 글</Link>
+            </nav>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/posts" element={<PostsPage />} />
                 <Route element={<ProtectedRouter />}>
+                    <Route path="/posts" element={<PostsPage />} />
+
                     <Route path="/post/new" element={<PostCreatePage />} />
                 </Route>
             </Routes>
