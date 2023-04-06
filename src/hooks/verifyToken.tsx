020952177246b4b91a779124b4e1882b8f040594
jyc-coder <jyc4648@gmail.com>
2023-04-06 22:17:11 +0900

    📝 feat : Add refreshResult in verifyToken
    - add refreshResult method in verifyToken

--- src/hooks/verifyToken.tsx
+++ src/hooks/verifyToken.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useQuery } from 'react-query'
-import { verify } from '../apis/services/auth'
-import { getCookie } from '../utils/cookies'
+import { refresh, verify } from '../apis/services/auth'
+import { getCookie, setCookie } from '../utils/cookies'
 
 type authType = 'PENDING' | 'SUCCESS' | 'FAILED'
 
@@ -18,6 +18,19 @@ function verifyToken() {
         enabled: !!getCookie('accessToken'), // 토큰이 있을 때만 verify
     }) // 쿠키가 없으면 undefined라서, boolean 타입으로 변환하기 위해서 !!를 사용한다.
 
+    // 액세스 토큰이 없을 때만
+    // 가능하다면, refresh token의 존재 여부를 쿠키 혹은 redux 상태로 관리하면서 함께 써주는 것이 좋다.
+    const refreshResult = useQuery(['auth', 'refresth'], refresh, {
+        onSuccess: (data) => {
+            setCookie('accessToken', data.accessToken, { path: '/', maxAge: data.exp - data.iat })
+            setIsAuthenticated('SUCCESS')
+        },
+        onError: () => {
+            setIsAuthenticated('FAILED')
+        },
+        retry: 0,
+        enabled: !getCookie('accessToken'),
+    })
     return isAuthenticated
 }
 
